268ae089dc8b8dcd56a037b904ab83eb907f30f6
Josiah Kievit <josiahtesfu@gmail.com>
2025-07-30 16:09:10 -0700

    Project setup.

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Editor from './components/Editor.svelte';
+  let scratchText = '';
+</script>
+
+<main class="min-h-screen bg-white text-black">
+  <Editor bind:value={scratchText} />
+</main>
---
+++ src/components/Editor.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let value = '';
+</script>
+
+<div
+  contenteditable="true"
+  class="w-full h-full p-6 font-sans text-lg outline-none leading-relaxed whitespace-pre-wrap"
+  bind:innerText={value}
+></div>
